099feb37eca54ca75dac2e0cb12861a09aeebe8c
tssantos <tssantos@gmail.com>
2022-06-04 18:56:54 +0000

    Deploying to gh-pages from @ tssantos/memory-game@751964980a1d9ccbe59e033c4ce5489431790777 🚀

--- _next/static/02lHe3NPX33R93WiiXRmi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f994b1465de1c159.css","static/chunks/pages/index-f4ca0e73b4e78575.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
